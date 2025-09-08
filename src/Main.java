b72438c5cd0ebfedb7e049a1e7425cc2dec26d98
desa <DenisWillm@t-online.de>
2025-09-09 01:42:04 +0200

    Finished prototype
    First working ranking algorithm through console

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,24 @@ import static java.util.Collections.shuffle;
 public class Main {
     public static void main(String[] args) {
 
-        HashMap<Integer, String> episodes = new HashMap<Integer, String>();
+        HashMap<Integer, String> entries = new HashMap<Integer, String>();
         HashMap<Integer, String> descriptions = new HashMap<Integer, String>();
         try {
-            File myObj = new File("test.tsv");
+            File myObj = new File("shortTest.tsv");
             FileReader input = new FileReader(myObj);
             Scanner sc = new Scanner(input);
 
             String name;
             String desc;
-            int episodeNumber = 1;
+            int entryNumber = 1;
 
             sc.useDelimiter("\t");
             while (sc.hasNextLine()) {
                 name = sc.next();
-                episodes.put(episodeNumber, name);
+                entries.put(entryNumber, name);
                 desc = sc.nextLine();
-                descriptions.put(episodeNumber, desc.trim());
-                episodeNumber++;
+                descriptions.put(entryNumber, desc.trim());
+                entryNumber++;
             }
 
             sc.close();
@@ -39,50 +39,56 @@ public class Main {
 
         // Sets up what will be the final Array containing the rankings
         ArrayList<String> finalRankedList = new ArrayList<>();
+        ArrayList<String> tempRankedList = new ArrayList<>();
 
         // Sets up number array for randomization
-        List<Integer> whichEpisodeNext = new ArrayList<>();
+        List<Integer> whichEntryNext = new ArrayList<>();
 
         // Populates with numbers from 1 to amount of list entries
-        for (int i = 1; i <= episodes.size(); i++) {
-            whichEpisodeNext.add(i);
+        for (int i = 1; i <= entries.size(); i++) {
+            whichEntryNext.add(i);
         }
 
-        shuffle(whichEpisodeNext);
+        shuffle(whichEntryNext);
 
-        int rankNewEpisodeIndex = 0;
-        int alreadyRankedIndex = 0;
-        int middleEntryOfRanked;
+        int newEntryIndex = 0;
+        int rankedEntryIndex = 0;
+        int middleEntryOfRankedIndex = 0;
 
-        boolean needNewEpisode = true;
+        boolean needNewEntry = true;
 
-        String newEpisodeName = "";
-        String rankedEpisodeName = "";
+        String newEntryName = "";
+        String rankedEntryName;
 
-        while(finalRankedList.size() < episodes.size()) {
+        while(finalRankedList.size() < entries.size()) {
 
-            if (needNewEpisode) {
-                rankNewEpisodeIndex = whichEpisodeNext.remove(0);
-                newEpisodeName = episodes.get(rankNewEpisodeIndex);
-//                needNewEpisode = false;
+            if (needNewEntry) {
+                newEntryIndex = whichEntryNext.remove(0);
+                newEntryName = entries.get(newEntryIndex);
+                tempRankedList.addAll(finalRankedList);
+                needNewEntry = false;
             }
 
             // Adds newly chosen list entry to final ranking (First entry so no choice required)
             if (finalRankedList.isEmpty()) {
-                finalRankedList.add(episodes.get(rankNewEpisodeIndex));
+                finalRankedList.add(entries.get(newEntryIndex));
+                needNewEntry = true;
+                continue;
             }
 
             if (finalRankedList.size() == 1) {
-                rankedEpisodeName = finalRankedList.get(0);
+                rankedEntryName = finalRankedList.get(0);
             } else {
-                middleEntryOfRanked = (int) Math.ceil(finalRankedList.size() / 2.0);
 
-                alreadyRankedIndex = finalRankedList.indexOf(finalRankedList.get(middleEntryOfRanked));
-                rankedEpisodeName = finalRankedList.get(alreadyRankedIndex);
+                middleEntryOfRankedIndex = (int) Math.floor((tempRankedList.size() - 1) / 2.0);
+                rankedEntryName = tempRankedList.get(middleEntryOfRankedIndex);
+                rankedEntryIndex = tempRankedList.indexOf(rankedEntryName);
+
+                rankedEntryName = tempRankedList.get(rankedEntryIndex);
             }
 
-                System.out.println("Welche Folge ist die bessere, 1 oder 2?");
-                System.out.println(newEpisodeName + " oder " + rankedEpisodeName);
+            System.out.println("Welche Folge ist die bessere, 1 oder 2?");
+            System.out.println(newEntryName + " oder " + rankedEntryName);
 
             boolean decisionValid = false;
 
@@ -93,12 +99,37 @@ public class Main {
 
                 if (decision.equals("1")) {
 
-                    finalRankedList.add(finalRankedList.indexOf(finalRankedList.get(alreadyRankedIndex)) + 1, episodes.get(rankNewEpisodeIndex));
+                    if (tempRankedList.size() == 1) {
+                        finalRankedList.add(finalRankedList.indexOf(rankedEntryName) + 1, entries.get(newEntryIndex));
+                        needNewEntry = true;
+                        tempRankedList.clear();
+                    } else {
+
+                        for (int i = tempRankedList.size(); i >= 0; i--) {
+                            if (rankedEntryIndex >= i) {
+                                tempRankedList.remove(i);
+                            }
+                        }
+
+                    }
+
                     decisionValid = true;
 
                 } else if (decision.equals("2")) {
 
-                    finalRankedList.add(finalRankedList.indexOf(finalRankedList.get(alreadyRankedIndex)), episodes.get(rankNewEpisodeIndex));
+                    if (middleEntryOfRankedIndex == 0) {
+                        finalRankedList.add(finalRankedList.indexOf(finalRankedList.get(rankedEntryIndex)), entries.get(newEntryIndex));
+                        needNewEntry = true;
+                        tempRankedList.clear();
+                    } else {
+
+                        for (int i = tempRankedList.size() - 1; i >= 0; i--) {
+                            if (rankedEntryIndex <= i) {
+                                tempRankedList.remove(i);
+                            }
+                        }
+                    }
+
                     decisionValid = true;
 
                 } else {
@@ -108,9 +139,8 @@ public class Main {
                 }
             }
 
-            System.out.println(finalRankedList);
+            System.out.println("fRL content: " + finalRankedList);
         }
-
 
     }
 }
